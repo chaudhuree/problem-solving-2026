efdb01010d3c6974f1acdfe7600bb213cfb1b616
chaudhuree <chaudhuree05@gmail.com>
2026-03-03 21:38:15 +0600

    added task.json file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+    // ifstream cin("input.txt");
+    // ofstream cout("output.txt");
+    int i;
+    cin >> i;
+    cout << "Hello, " << i << endl;
+
+    return 0;
+}
