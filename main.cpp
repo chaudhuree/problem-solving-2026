c64828f27cb88ecd8e7a4349b0062e55d8dbd0ab
chaudhuree <chaudhuree05@gmail.com>
2026-03-06 01:37:55 +0600

    refactor: update program logic to print a diamond shape based on user input

--- main.cpp
+++ main.cpp
@@ -1,25 +1,44 @@
 /*
  * Author: Shahriar Kabir Sohan
  * github: chaudhuree
- * Program: password fix
- * https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+ * Program: W. Shape3
+ * https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/W
  */
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int X;
-
-    while (cin >> X)
+    int N;
+    cin >> N;
+    // printing upper half of the diamond
+    for (int i = 1; i <= N; i++)
     {
-        if (X == 1999)
+        for (int j = 1; j <= N - i; j++)
         {
-            cout << "Correct";
-            break;
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // printing lower half of the diamond
+    for (int i = N; i >= 1; i--)
+    {
+        for (int j = 1; j <= N - i; j++)
+        {
+            cout << " ";
         }
 
-        cout << "Wrong" << endl;
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
     }
 
     return 0;
