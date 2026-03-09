350d4c23e406d39272c750f250ffd74410faf64a
chaudhuree <chaudhuree00@gmail.com>
2026-03-09 10:59:48 +0600

    refactor: update program logic to remove diamond shape printing and adjust array handling

--- main.cpp
+++ main.cpp
@@ -1,45 +1,24 @@
 /*
  * Author: Shahriar Kabir Sohan
  * github: chaudhuree
- * Program: W. Shape3
- * https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/W
  */
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    // printing upper half of the diamond
-    for (int i = 1; i <= N; i++)
+    int arr[] = {1, 3, 5, 4, 9};
+    int pos = 2;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i = pos; i < n-1; i++)
     {
-        for (int j = 1; j <= N - i; j++)
-        {
-            cout << " ";
-        }
-        for (int k = 1; k <= 2 * i - 1; k++)
-        {
-            cout << "*";
-        }
-        cout << endl;
+        arr[i] = arr[i+1];
     }
-
-    // printing lower half of the diamond
-    for (int i = N; i >= 1; i--)
+    n--;
+    for(int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= N - i; j++)
-        {
-            cout << " ";
-        }
-
-        for (int j = 1; j <= 2 * i - 1; j++)
-        {
-            cout << "*";
-        }
-
-        cout << endl;
+        cout << arr[i] << " ";
     }
-
     return 0;
 }
