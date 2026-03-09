af0759bc4bab57ea4f267d3ee459a58908eef433
chaudhuree <chaudhuree00@gmail.com>
2026-03-09 15:32:08 +0600

    refactor: update program logic to count consecutive duplicates and adjust input/output handling

--- main.cpp
+++ main.cpp
@@ -6,21 +6,20 @@
 using namespace std;
 
 int main() {
-
-    int arr[5] = {1,2,3,4,5};
-    int n = 5;
-
-    int left = 0;
-    int right = n-1;
-
-    while(left < right){
-
-        swap(arr[left], arr[right]);
-
-        left++;
-        right--;
+    int length;
+    cin >> length;
+    int remove_count =0;
+    char* arr = new char[length];
+    for (int i = 0; i < length; i++) {
+        cin >> arr[i];
     }
-
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
+    for(int i = 0; i<length; i++) {
+        if(arr[i]== arr[i+1]){
+            remove_count++;
+        }
+        
+        
+    }
+    cout << remove_count;
+    return 0;
 }
